179216f5212792bfe8bbb7c909e1475a3bf48db2
Anatoly Ryzhkov <Ryzhkov.Anatoly@gmail.com>
2022-02-27 13:59:34 +0500

    Сделал фукции в main.js стрелочными

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-function getRandomInteger (beginInteger, endInteger) {
-  const rangeInteger = Math.abs(endInteger - beginInteger) + 1;
+const getRandomInteger = (beginInteger, endInteger) => {
+  const rangeInteger = Math.abs(Math.abs(endInteger) - Math.abs(beginInteger)) + 1;
   return Math.floor(Math.random() * (rangeInteger) + Math.min(beginInteger, endInteger));
-}
+};
 
 getRandomInteger(2,5);
 
-function checkLengthString (stringCheck, maxLengthString) {
-  return maxLengthString >= stringCheck.length;
-}
+const checkLengthString = (stringCheck, maxLengthString) => maxLengthString >= stringCheck.length;
 
 checkLengthString('Ryzhkov Anatoly', 9);
