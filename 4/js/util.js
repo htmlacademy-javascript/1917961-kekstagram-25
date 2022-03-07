9a031e6dfbec821db9760096d7622b77d94ee59e
Keks <keks@htmlacademy.ru>
2022-03-07 11:38:05 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/util.js
@@ -0,0 +1,17 @@
+const getRandomInteger = (beginInteger, endInteger) => {
+  const rangeInteger = Math.abs(Math.abs(endInteger) - Math.abs(beginInteger)) + 1;
+  return Math.floor(Math.random() * (rangeInteger) + Math.min(beginInteger, endInteger));
+};
+
+const checkLengthString = (stringCheck, maxLengthString) => maxLengthString >= stringCheck.length;
+
+const getRandomValueFromArray = (arrayFromGetValue) => arrayFromGetValue[getRandomInteger(0, arrayFromGetValue.length - 1)];
+
+const createArrayIndexInValue = (count) => Array.from({ length: count }, (value, key) => key + 1);
+
+const deleteRandomValueFromArray = (arrayFromGetIdentifier) => {
+  const index = getRandomInteger(0, arrayFromGetIdentifier.length - 1);
+  return arrayFromGetIdentifier.splice(index, 1)[0];
+};
+
+export { getRandomInteger, checkLengthString, getRandomValueFromArray, createArrayIndexInValue, deleteRandomValueFromArray };
