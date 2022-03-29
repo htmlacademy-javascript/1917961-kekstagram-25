aad112fef485a8c269c7312f74bd8860d1cf9eb5
Anatoly Ryzhkov <Ryzhkov.Anatoly@gmail.com>
2022-03-29 13:54:18 +0500

    Сделал загрузку изображений

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-import './previewing.js';
 import './upload-image.js';
+import {previewImages} from './previewing.js';
+//import './previewing.js';
+
+fetch('https://25.javascript.pages.academy/kekstagram/data')
+  .then((response) => response.json())
+  .then((data) => {
+    //console.log(data);
+    previewImages(data);
+  });
--- js/previewing.js
+++ js/previewing.js
@@ -1,20 +1,24 @@
-import { createPhotoDescriptions } from './data.js';
+//import { createPhotoDescriptions } from './data.js';
 import { addHandlerClickPreviews } from './full-viewing.js';
 import { CONTAINER_IMAGES_OTHER_USERS } from './const.js';
 
 const templatePreview = document.querySelector('#picture').content.querySelector('.picture');
-const previewsElementDescriptions = createPhotoDescriptions();
+//const previewsElementDescriptions = createPhotoDescriptions();
 const previewsFragment = document.createDocumentFragment();
 
-previewsElementDescriptions.forEach((previewsElementDescription) => {
-  const previewsElement = templatePreview.cloneNode(true);
-  const pictureImg = previewsElement.querySelector('.picture__img');
-  pictureImg.src = previewsElementDescription.url;
-  pictureImg.alt = previewsElementDescription.description;
-  previewsElement.querySelector('.picture__comments').textContent = String(previewsElementDescription.comments.length);
-  previewsElement.querySelector('.picture__likes').textContent = String(previewsElementDescription.likes);
-  addHandlerClickPreviews(previewsElement, previewsElementDescription);
-  previewsFragment.appendChild(previewsElement);
-});
+const previewImages = (previewsElementDescriptions) => {
+  previewsElementDescriptions.forEach((previewsElementDescription) => {
+    const previewsElement = templatePreview.cloneNode(true);
+    const pictureImg = previewsElement.querySelector('.picture__img');
+    pictureImg.id = previewsElementDescription.id;
+    pictureImg.src = previewsElementDescription.url;
+    pictureImg.alt = previewsElementDescription.description;
+    previewsElement.querySelector('.picture__comments').textContent = String(previewsElementDescription.comments.length);
+    previewsElement.querySelector('.picture__likes').textContent = String(previewsElementDescription.likes);
+    //addHandlerClickPreviews(previewsElement, previewsElementDescription);
+    previewsFragment.appendChild(previewsElement);
+  });
+  CONTAINER_IMAGES_OTHER_USERS.appendChild(previewsFragment);
+};
 
-CONTAINER_IMAGES_OTHER_USERS.appendChild(previewsFragment);
+export {previewImages};
